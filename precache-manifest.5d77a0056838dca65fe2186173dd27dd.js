5d3540f0568709438cbe006f5879b9b7964dc1e1
JIACHENG135 <liulaoye135@tamu.edu>
2020-10-10 16:50:22 -0500

    ready. to deploy

---
+++ precache-manifest.5d77a0056838dca65fe2186173dd27dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451b9c654b46f513d6fe244187e769a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7340f92b01d0781e44c",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "712e0cd5ad41ea958aa0",
+    "url": "/static/css/main.e3caeecf.chunk.css"
+  },
+  {
+    "revision": "d7340f92b01d0781e44c",
+    "url": "/static/js/2.22125eac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.22125eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712e0cd5ad41ea958aa0",
+    "url": "/static/js/main.fb6b4da1.chunk.js"
+  },
+  {
+    "revision": "4bcb0f8a0ed1ddd67738",
+    "url": "/static/js/runtime-main.cbf65df0.js"
+  }
+]);
